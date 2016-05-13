5a8b5524abb03b86bc5dcee266bf7845726ad48c
Jim_Stockman <jimstockman44@gmail.com>
2016-05-13 13:02:47 +0200

    Password sandbox

---
+++ Page protégée par mot de passe.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	
+	<title>Page protégée par mot de passe</title>
+
+</head>
+
+<body>
+	<form method="post" action="target.php">
+		<p>
+			<input type="password" name="MDP">
+			<input type="submit" name="confirmer">
+		</p>
+	</form>
+</body>
+
+</html>
