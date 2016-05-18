a3d4d55b86e69af674175345203272e8b8c9fb9c
Jim_Stockman <jimstockman44@gmail.com>
2016-05-18 19:48:24 +0200

    ajout gerex
    à relire pour utilisation future

---
+++ call.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<!-- fichier PHP à placer entre les guillemets -->
+<?php include(""); ?>
+</body>
+</html>
---
+++ exp_gener.php
@@ -0,0 +1,10 @@
+<?php
+if (preg_match("#guitare#", "J'aime jouer de la guitare."))
+{
+    echo 'VRAI';
+}
+else
+{
+    echo 'FAUX';
+}
+?>
