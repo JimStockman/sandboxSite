5854befa2e5574ca14101cd00dfc8a376d4c43d1
Jim_Stockman <jimstockman44@gmail.com>
2016-05-19 13:49:15 +0200

    POO
    à relire !ATTENTIVEMENT!

---
+++ Membre.class.php
@@ -0,0 +1,38 @@
+<?php
+class Membre
+{
+private $pseudo;
+private $email;
+private $signature;
+private $actif;
+
+public function envoyerEMail($titre, $message)
+    {
+        mail($this->email, $titre, $message);
+    }
+    
+public function bannir()
+    {
+        $this->actif = false;
+        $this->envoyerEMail('Vous avez été banni', 'Ne revenez plus !');
+    }
+    
+public function getPseudo()
+{
+	return $this->pseudo;
+}
+
+public function setPseudo($nouveauPseudo)
+{
+	  // Vérifier si le nouveau pseudo n'est ni vide ni trop long
+    if (!empty($nouveauPseudo) AND strlen($nouveauPseudo) < 15)
+    {
+        // Ok, on change son pseudo
+        $this->pseudo = $nouveauPseudo;
+    }
+}
+
+
+/* et ainsi de suite */
+}
+?>
