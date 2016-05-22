08b8f443c3e095d4c404fa42e54e3c9d6dadd262
STRYK3R23 <jimstockman44@gmail.com>
2016-05-22 23:40:19 +0200

    Bootstrap, premier pas

--- call.php
+++ call.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-<!-- fichier PHP à placer entre les guillemets -->
-<?php include(""); ?>
-</body>
+  <head>
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="tuto.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">4 colonnes</div>
+        <div class="col-lg-8">8 colonnes</div>
+      </div>
+    </div>
+  </body>
 </html>
